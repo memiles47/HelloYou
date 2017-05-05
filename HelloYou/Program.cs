55e0fd469d60a8dde1ded32cd2520f904b2ef407
Michael E Miles <michael@miles-me.net>
2017-05-05 14:34:15 -0500

    Initial Commit

--- HelloYou/Program.cs
+++ HelloYou/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HelloYou
 {
@@ -10,6 +6,34 @@ namespace HelloYou
     {
         static void Main(string[] args)
         {
+            //Initialize variables
+            string name;
+            string age;
+            string city;
+            string love;
+            string output;
+
+            //Ask user for name
+            Console.WriteLine("What is your name?: ");
+            name = Console.ReadLine();
+
+            //Ask user for age
+            Console.WriteLine("How old are you?:");
+            age = Console.ReadLine();
+
+            //Ask user for city
+            Console.WriteLine("What cidy do you live in?:");
+            city = Console.ReadLine();
+
+            //Ask user what they enjoy
+            Console.WriteLine("What do you love doing?:");
+            love = Console.ReadLine();
+
+            //Create output text
+            //output = 
+
+
+            //Print output to screen
         }
     }
 }
